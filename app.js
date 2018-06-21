30f125fda0a8b70f996606cb782a65f0043c28c4
Krishna <mitkrishnakumar@gmail.com>
2018-06-22 00:06:51 +0530

    shop cart finished

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 var fs = require('fs');
 var mongoose = require('mongoose');
+var expressHbs = require('express-handlebars');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
 var keys = require('./config/keys');
 var app = express();
 
@@ -19,14 +25,32 @@ var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
   flags: 'a'
 });
 
+require('./config/passport');
+
+// view engine setup
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: 'mysupersecret',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+  })
+);
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
